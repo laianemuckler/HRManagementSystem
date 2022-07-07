d871d4d86119f39d5208c24fdacc43f23d8c7bbe
Laiane Muckler <laiane.muckler@programmers.com.br>
2022-07-07 20:02:20 -0300

    adding a service layer

---
+++ HRManagementSystem/Services/Interfaces/IEmployeeService.cs
@@ -0,0 +1,14 @@
+﻿using HRManagementSystem.Dtos;
+using HRManagementSystem.Entities;
+
+namespace HRManagementSystem.Services.Interfaces
+{
+    public interface IEmployeeService
+    {
+        EmployeeDto GetEmployee(Guid id);
+        IEnumerable<EmployeeDto> GetEmployees();
+        void CreateEmployee(EmployeeDto employee);
+        void UpdateEmployee(EmployeeDto employee);
+        void DeleteEmployee(Guid id);
+    }
+}
---
+++ HRManagementSystem/Services/Services/EmployeeService.cs
@@ -0,0 +1,44 @@
+﻿using HRManagementSystem.Dtos;
+using HRManagementSystem.Repositories;
+using HRManagementSystem.Services.Interfaces;
+
+namespace HRManagementSystem.Services.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeService(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public void CreateEmployee(EmployeeDto employee)
+        {
+            // validações 
+            // após validações enviar para o repository
+            _employeeRepository.CreateEmployee()
+        }
+
+        public void DeleteEmployee(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EmployeeDto GetEmployee(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<EmployeeDto> GetEmployees()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateEmployee(EmployeeDto employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
